f0569123ad618c37b9fbc3b62bd8749338a0d56b
rafatalahmad <155534517+rafatalahmad@users.noreply.github.com>
2025-10-20 16:18:48 +0300

    Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
+        public function Register(Request $request)
+    {
+        $request->validate([
+             'name'=>'required|string|max:40',
+             'email'=>'required|string|email|max:50|unique:users,email',
+             'password'=>'required|string|confirmed|min:8'
+             
+        ]);
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+
+        ]);
+        return response()->json('ok',201);
+
+    }
 }
